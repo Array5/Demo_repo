f304510af53f5a2e5cac1b6034d578292922e641
Array5 <108741136+Array5@users.noreply.github.com>
2022-07-05 19:30:16 +0530

    Third_commit

--- MyFirstPgm.py
+++ MyFirstPgm.py
@@ -1,10 +1,12 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.edge.service import Service
+from selenium.webdriver.opera.service import Service
 
-s=Service(executable_path="C:\\Users\\91959\\Downloads\\chromedriver_win32 (2)\\msedgedriver.exe")
-driver=webdriver.Edge(service=s)
+s=Service(executable_path="C:\\Users\\91959\\Downloads\\chromedriver_win32 (2)\\operadriver.exe")
+driver=webdriver.Opera(service=s)
 driver.get("http://www.fb.com")
 driver.maximize_window()
+actual_title=driver.title
+print(actual_title)
 diver.close()
