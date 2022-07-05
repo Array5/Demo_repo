e94067f4541af8ea0c1201526f09e5c082737276
Array5 <108741136+Array5@users.noreply.github.com>
2022-07-05 19:41:57 +0530

    update demo_code_initial_commit
    demo purpose

--- MyFirstPgm.py
+++ MyFirstPgm.py
@@ -7,8 +7,10 @@ s=Service(executable_path="C:\\Users\\91959\\Downloads\\chromedriver_win32 (2)\\
 driver=webdriver.Opera(service=s)
 driver.get("http://www.fb.com")
 driver.maximize_window()
+driver.implicitly_wait(30)
 actual_title=driver.title
 print(actual_title)
 actual_url=driver.current_url
 print(actual_url)
+driver.swithch_to.alert.accept()
 diver.close()
