27d2a451e14187c5992e1e97d21dd6cf12ea0e7c
Array5 <108741136+Array5@users.noreply.github.com>
2022-07-05 19:28:38 +0530

    Second_commit
    change edge browser

--- MyFirstPgm.py
+++ MyFirstPgm.py
@@ -1,10 +1,10 @@
 import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.edge.service import Service
 
-s=Service(executable_path="C:\\Users\\91959\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe")
-driver=webdriver.Chrome(service=s)
+s=Service(executable_path="C:\\Users\\91959\\Downloads\\chromedriver_win32 (2)\\msedgedriver.exe")
+driver=webdriver.Edge(service=s)
 driver.get("http://www.fb.com")
 driver.maximize_window()
 diver.close()
